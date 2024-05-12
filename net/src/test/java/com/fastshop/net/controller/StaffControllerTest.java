eec39fbd24371962696fc531824ae6990df69d5a
hongvan <128266763+DuongThiHongVan@users.noreply.github.com>
2024-05-12 22:06:44 +0700

    Add files via upload

---
+++ net/src/test/java/com/fastshop/net/controller/StaffControllerTest.java
@@ -0,0 +1,65 @@
+package com.fastshop.net.controller;
+
+import org.junit.jupiter.api.Test;
+
+public class StaffControllerTest {
+    @Test
+    void testCategory() {
+
+    }
+
+    @Test
+    void testChange() {
+
+    }
+
+    @Test
+    void testConfirm() {
+
+    }
+
+    @Test
+    void testDetails() {
+
+    }
+
+    @Test
+    void testDicsount() {
+
+    }
+
+    @Test
+    void testGetAuth() {
+
+    }
+
+    @Test
+    void testHome() {
+
+    }
+
+    @Test
+    void testProduct() {
+
+    }
+
+    @Test
+    void testProducts() {
+
+    }
+
+    @Test
+    void testRedetail() {
+
+    }
+
+    @Test
+    void testReport() {
+
+    }
+
+    @Test
+    void testStatus() {
+
+    }
+}
