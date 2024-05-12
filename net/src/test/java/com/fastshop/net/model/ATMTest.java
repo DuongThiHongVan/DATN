c0babf667a75a1bcb5db5726b842afeb502ee6e8
hongvan <128266763+DuongThiHongVan@users.noreply.github.com>
2024-05-12 10:57:22 +0700

    Add files via upload

---
+++ net/src/test/java/com/fastshop/net/model/ATMTest.java
@@ -0,0 +1,85 @@
+package com.fastshop.net.model;
+
+import org.junit.jupiter.api.Test;
+
+public class ATMTest {
+    @Test
+    void testCanEqual() {
+
+    }
+
+    @Test
+    void testEquals() {
+
+    }
+
+    @Test
+    void testGetAccount() {
+
+    }
+
+    @Test
+    void testGetCompany() {
+
+    }
+
+    @Test
+    void testGetId() {
+
+    }
+
+    @Test
+    void testGetName() {
+
+    }
+
+    @Test
+    void testGetNumber() {
+
+    }
+
+    @Test
+    void testGetValid() {
+
+    }
+
+    @Test
+    void testHashCode() {
+
+    }
+
+    @Test
+    void testSetAccount() {
+
+    }
+
+    @Test
+    void testSetCompany() {
+
+    }
+
+    @Test
+    void testSetId() {
+
+    }
+
+    @Test
+    void testSetName() {
+
+    }
+
+    @Test
+    void testSetNumber() {
+
+    }
+
+    @Test
+    void testSetValid() {
+
+    }
+
+    @Test
+    void testToString() {
+
+    }
+}
